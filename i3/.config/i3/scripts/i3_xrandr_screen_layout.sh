7383026f7b00411cc85a9228d7ea173b5d37f59f
jkurthoconnor <jkurthoconnor@gmail.com>
2018-07-22 21:58:11 -0600

    init commit: core i3 config and style

---
+++ i3/.config/i3/scripts/i3_xrandr_screen_layout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DVI-D-0 --primary --mode 1920x1080 --pos 0x200 --rotate normal --output DP-1 --mode 1600x900 --pos 1920x0 --rotate left --output HDMI-0 --off
