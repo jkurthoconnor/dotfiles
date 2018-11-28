1054a7952eab672387f7889daf1ee4c2275c6c64
jkurthoconnor <jkurthoconnor@gmail.com>
2018-11-27 21:19:17 -0700

    Reconfigure for portrait orientation

--- i3/.config/i3/scripts/i3_xrandr_screen_layout.sh
+++ i3/.config/i3/scripts/i3_xrandr_screen_layout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-0 --off --output DVI-D-0 --primary --mode 1920x1080 --pos 0x200 --rotate normal --output DP-1 --mode 1600x900 --pos 1920x0 --rotate left --output HDMI-0 --off
+xrandr --output DP-0 --mode 1920x1200 --pos 1920x0 --rotate right --output DVI-D-0 --off --output DP-1 --off --output HDMI-0 --primary --mode 1920x1200 --pos 0x328 --rotate normal
