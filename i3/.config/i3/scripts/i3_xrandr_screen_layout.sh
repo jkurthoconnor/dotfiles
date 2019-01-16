1904af21021d1bbbc4bd20aeac8921173ce82c7f
jkurthoconnor <jkurthoconnor@gmail.com>
2019-01-16 15:13:19 -0700

    Set xrandr options by hostname

--- i3/.config/i3/scripts/i3_xrandr_screen_layout.sh
+++ i3/.config/i3/scripts/i3_xrandr_screen_layout.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
-xrandr --output DP-0 --mode 1920x1200 --pos 1920x0 --rotate right --output DVI-D-0 --off --output DP-1 --off --output HDMI-0 --primary --mode 1920x1200 --pos 0x328 --rotate normal
+#!/bin/bash
+
+device=$HOSTNAME
+desktop=sullilly
+laptop=kell
+
+if [ $device = $desktop ]; then
+  xrandr --output DP-0 --mode 1920x1200 --pos 1920x0 --rotate right --output DVI-D-0 --off --output DP-1 --off --output HDMI-0 --primary --mode 1920x1200 --pos 0x328 --rotate normal
+fi
