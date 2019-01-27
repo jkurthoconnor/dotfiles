9713a08203f42da1fc2e904487063e5b1b7b3e20
jkurthoconnor <jkurthoconnor@gmail.com>
2019-01-27 13:10:33 -0700

    Clean up: add exit status and expansion suppression

--- i3/.config/i3/scripts/intel-screen-brightness.sh
+++ i3/.config/i3/scripts/intel-screen-brightness.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 
+# chown root for this script
+# run script with `sudo` to write brightness setting
+# may need to modify sudoers (`sudo visudo`) & include arguments in script call
+
+SETTINGS=/sys/class/backlight/intel_backlight/brightness
 MAX=$(cat /sys/class/backlight/intel_backlight/max_brightness)
 MIN=1000
 
-current=$(cat /sys/class/backlight/intel_backlight/brightness)
+current=$(cat "$SETTINGS")
 new="$current"
 
 if [ "$1" = "-inc" ]; then
-  new=$[$current + 2000]
+  new=$["$current" + 2000]
 elif [ "$1" = "-dec" ]; then
-  new=$[$current - 2000]
+  new=$["$current" - 2000]
+else
+  exit 1
 fi
 
 if [ "$new" -le "$MAX" ] && [ "$new" -ge "$MIN" ]; then
-  echo $new > /sys/class/backlight/intel_backlight/brightness
+  echo "$new" > "$SETTINGS"
+  exit 0
 fi
