999dfe24b6e4437e1ddfeb2d2dcc19487e5e06e1
jkurthoconnor <jkurthoconnor@gmail.com>
2019-02-09 21:44:58 -0700

    Update names in workspace scripts to follow config

--- i3/.config/i3/scripts/i3_predefined_workspace_layouts.sh
+++ i3/.config/i3/scripts/i3_predefined_workspace_layouts.sh
@@ -6,10 +6,10 @@ laptop=kell
 
 if [ $device = $desktop ]; then
   # load pre-defined layouts
-  i3-msg "workspace 4. code; append_layout /home/jko/.config/i3/layouts/ws4.json"
-  i3-msg "workspace 10. misc; append_layout /home/jko/.config/i3/layouts/ws0.json"
+  i3-msg "workspace 4 cde; append_layout /home/jko/.config/i3/layouts/ws4.json"
+  i3-msg "workspace 0 etc; append_layout /home/jko/.config/i3/layouts/ws0.json"
   # open client instance assigned to workspace
-  i3-msg "workspace 8. comm; exec firefox https://mail.google.com"
+  i3-msg "workspace 8 com; exec firefox https://mail.google.com"
   # open client assigned to scratchpad
   i3-msg "exec --no-startup-id keepassxc"
   # open clients initially assigned to pre-defined layouts
@@ -19,6 +19,8 @@ if [ $device = $desktop ]; then
   i3-msg "exec termite"
   i3-msg "exec termite"
 elif [ $device = $laptop ]; then
+  # open client instance assigned to workspace
+  i3-msg "workspace 8 com; exec firefox https://mail.google.com"
   i3-msg "exec termite"
   i3-msg "exec termite"
 fi
