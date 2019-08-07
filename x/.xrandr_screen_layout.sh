7912c61a1b12c0707015eb6339ade54ee1d7e710
jkurthoconnor <jkurthoconnor@gmail.com>
2019-08-06 22:44:13 -0600

    Additions for portable monitor & laptop nvidia

--- x/.xrandr_screen_layout.sh
+++ x/.xrandr_screen_layout.sh
@@ -4,6 +4,11 @@ device=$HOSTNAME
 desktop=sullilly
 laptop=kell
 
-if [ $device = $desktop ]; then
+if [ $device == $desktop ]; then
   xrandr --output DP-0 --mode 1920x1200 --pos 1920x0 --rotate right --output DVI-D-0 --off --output DP-1 --off --output HDMI-0 --primary --mode 1920x1200 --pos 0x328 --rotate normal
+elif [ $device == $laptop ]; then
+  system76-power graphics | grep -q 'nvidia'
+  if [ $? -eq 0 ]; then
+    xrandr --output eDP-1-1 --auto --pos 0x0 --primary --output DVI-I-2-1 --auto --pos 1920x0 --right-of eDP-1-1 --rotate left
+  fi
 fi
