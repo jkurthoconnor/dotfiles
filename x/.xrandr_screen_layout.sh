4b0a59135592d54869e96ed017e3b840d826aa2d
jkurthoconnor <jkurthoconnor@gmail.com>
2019-11-17 17:43:38 -0700

    Rotate desktop screen opposite direction

--- x/.xrandr_screen_layout.sh
+++ x/.xrandr_screen_layout.sh
@@ -5,7 +5,7 @@ desktop=sullilly
 laptop=kell
 
 if [ $device == $desktop ]; then
-  xrandr --output DP-0 --mode 1920x1200 --pos 1920x0 --rotate right --output DVI-D-0 --off --output DP-1 --off --output HDMI-0 --primary --mode 1920x1200 --pos 0x328 --rotate normal
+  xrandr --output DP-0 --mode 1920x1200 --pos 1920x0 --rotate left --output DVI-D-0 --off --output DP-1 --off --output HDMI-0 --primary --mode 1920x1200 --pos 0x328 --rotate normal
 elif [ $device == $laptop ]; then
   system76-power graphics | grep -q 'nvidia'
   if [ $? -eq 0 ]; then
