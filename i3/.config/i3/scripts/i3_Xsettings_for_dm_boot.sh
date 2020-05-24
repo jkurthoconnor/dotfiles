524fcb1b6f0ae6b939373212541c7dee9d9e9954
jkurthoconnor <jkurthoconnor@gmail.com>
2020-05-23 20:46:10 -0400

    Capture and float nautilus; boot to getty fixes

--- i3/.config/i3/scripts/i3_Xsettings_for_dm_boot.sh
+++ i3/.config/i3/scripts/i3_Xsettings_for_dm_boot.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # `.xinitrc` is bypassed when Xserver is started by a display manager
-# so this script ensures proper settings for display manager X starts by
-# excluding non-dm boxes
+# This script ensures proper settings for dm X starts
+# TODO: Xresources are available only after i3 draws the screen, so some colors require a config reload to display; call script earlier for dm target boots
 
-# desktop=^sull
+desktop=^sull
 laptop=^kel
 
-# [[ $HOSTNAME =~ $desktop || $HOSTNAME =~ $laptop ]] && exit 0
-[[ $HOSTNAME =~ $laptop ]] && exit 0
+# exit early for non-dm boot devices (.xinitrc will have already run)
+[[ $HOSTNAME =~ $desktop || $HOSTNAME =~ $laptop ]] && exit 0
 
 [[ -f ~/.Xresources ]] && xrdb -merge -I$HOME ~/.Xresources
 source $HOME/.xrandr_screen_layout.sh
--- i3/.config/i3/scripts/i3_predefined_workspace_layouts.sh
+++ i3/.config/i3/scripts/i3_predefined_workspace_layouts.sh
@@ -4,7 +4,6 @@ desktop=^sull
 laptop=^kel
 browser=firefox
 terminal=termite
-layout1="/home/jko/.config/i3/layouts/default-terminal-split.json"
 
 if [[ $HOSTNAME =~ $desktop ]]; then
   terminal=alacritty
@@ -14,18 +13,10 @@ if [[ ! $HOSTNAME =~ $desktop && ! $HOSTNAME =~ $laptop ]]; then
   browser=google-chrome
 fi
 
-# load common layouts into numbered workspaces
-for n in 1 2 7; do
-  i3-msg "workspace $n:trm; append_layout ${layout1}"
-done
-
-# open clients to be initially 'captured' by pre-defined layouts
-for n in $(seq 6); do
-  i3-msg "exec ${terminal}"
-done
+# open client instances in specified workspaces
+i3-msg "workspace 8:com; exec ${browser} https://mail.google.com"
+i3-msg "workspace 0:trm; exec ${terminal}"
 
 # open clients to be 'captured' by scratchpad
 i3-msg "exec ${terminal} --title=scratchterm"
 
-# open client instances in specified workspaces
-i3-msg "workspace 8:com; exec ${browser} https://mail.google.com"
