ac8004e7ff104213b9be6d76d1e200b133b6fbe9
jkurthoconnor <jkurthoconnor@gmail.com>
2020-07-31 14:05:18 -0400

    Add portrait|portrait orientation

--- x/.xrandr_screen_layout.sh
+++ x/.xrandr_screen_layout.sh
@@ -5,10 +5,11 @@ device=$HOSTNAME
 desktop=^su
 laptop=^ke
 
-# if [[ $device =~ $desktop ]]; then
-  # xrandr --output DP-0 --mode 1920x1200 --pos 1920x0 --rotate right --output DVI-D-0 --off --output DP-1 --off --output HDMI-0 --primary --mode 1920x1200 --pos 0x328 --rotate normal
 if [[ $device =~ $desktop ]]; then
-  xrandr --output DP-1 --mode 1920x1200 --pos 1920x0 --rotate right --output DVI-D-1 --off --output HDMI-1 --primary --mode 1920x1200 --pos 0x328 --rotate normal
+  # portrait | portrait
+  xrandr --output DP-1 --mode 1920x1200 --pos 1200x0 --rotate right --output DVI-D-1 --off --output HDMI-1 --primary --mode 1920x1200 --pos 0x0 --rotate left
+  # landscape | portrait
+  # xrandr --output DP-1 --mode 1920x1200 --pos 1920x0 --rotate right --output DVI-D-1 --off --output HDMI-1 --primary --mode 1920x1200 --pos 0x328 --rotate normal
 elif [[ $device =~ $laptop ]]; then
   system76-power graphics | grep -q 'nvidia'
   if [ $? -eq 0 ]; then
