5c6748becab21a68647fba7220333771a56d1085
jkurthoconnor <jkurthoconnor@gmail.com>
2020-10-31 20:12:45 -0400

    Accept args to set landscape/portrait

--- x/.xrandr_screen_layout.sh
+++ x/.xrandr_screen_layout.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 # executed  by .xinitrc (for getty login) and sourced in i3_Xsettings_for_dm_boot.sh
+# When called directly and device is $desktop, passing in first argument as anything but
+# 'landscape' will set the left monitor to portrait mode
 
 device=$HOSTNAME
 desktop=^su
 laptop=^ke
+left_orientation=${1:-'landscape'}
 
+echo $left_orientation
 if [[ $device =~ $desktop ]]; then
-  # portrait | portrait
-  xrandr --output DP-1 --mode 1920x1200 --pos 1200x0 --rotate right --output DVI-D-1 --off --output HDMI-1 --primary --mode 1920x1200 --pos 0x0 --rotate left
   # landscape | portrait
-  # xrandr --output DP-1 --mode 1920x1200 --pos 1920x0 --rotate right --output DVI-D-1 --off --output HDMI-1 --primary --mode 1920x1200 --pos 0x328 --rotate normal
+  if [ $left_orientation == 'landscape' ]; then
+    xrandr --output DP-1 --mode 1920x1200 --pos 1920x0 --rotate right --output DVI-D-1 --off --output HDMI-1 --primary --mode 1920x1200 --pos 0x328 --rotate normal
+  # portrait | portrait
+  else
+    xrandr --output DP-1 --mode 1920x1200 --pos 1200x0 --rotate right --output DVI-D-1 --off --output HDMI-1 --primary --mode 1920x1200 --pos 0x0 --rotate left
+  fi
 elif [[ $device =~ $laptop ]]; then
   system76-power graphics | grep -q 'nvidia'
   if [ $? -eq 0 ]; then
