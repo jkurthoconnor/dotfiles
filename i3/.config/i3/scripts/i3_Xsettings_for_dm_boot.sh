f9222286339c98c22ac9a9c156f59e5f58d07718
jkurthoconnor <jkurthoconnor@gmail.com>
2020-10-31 20:39:01 -0400

    Improve TODO descriptions

--- i3/.config/i3/scripts/i3_Xsettings_for_dm_boot.sh
+++ i3/.config/i3/scripts/i3_Xsettings_for_dm_boot.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # `.xinitrc` is bypassed when Xserver is started by a display manager
-# This script ensures proper settings for dm X starts
-# TODO: Xresources are available only after i3 draws the screen, so some colors require a config reload to display; call script earlier for dm target boots
+# This script ensures proper x settings for dm triggered X starts
+# TODO: this script is called in the i3 start up, thus Xresources are being made available
+# only after i3 draws the screen; but i3 config assigns some colors from Xresources.
+# Thus some colors require a config reload to display; call script earlier for dm target boots
 
 desktop=^sull
 laptop=^kel
 
 # exit early for non-dm boot devices (.xinitrc will have already run)
+# TODO: determine if .xinitrc has already been called; do not rely on $HOSTNAME 
 [[ $HOSTNAME =~ $desktop || $HOSTNAME =~ $laptop ]] && exit 0
 
 [[ -f ~/.Xresources ]] && xrdb -merge -I$HOME ~/.Xresources
